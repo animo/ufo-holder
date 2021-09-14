bd4d5117c409d89e689e196b2c3a9450957c5a95
Berend Sliedrecht <berend@animo.id>
2021-09-14 12:03:03 +0200

    feat: emergency bottomsheet
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

--- src/containers/credentials/screens/CredentialDetailScreen.tsx
+++ src/containers/credentials/screens/CredentialDetailScreen.tsx
@@ -1,14 +1,9 @@
-import type { BottomSheetModal } from '@gorhom/bottom-sheet'
-
-import { CredentialsThunks } from '@aries-framework/redux-store'
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { HeaderIconButton, ScrollViewPage } from '@components/lib'
+import { ScrollViewPage } from '@components/lib'
 import { CredentialMetadata, FormDetail } from '@internal/components'
-import { DeleteBottomSheet } from '@internal/components/DeleteBottomSheet'
 import { useAppStackNavigation } from '@internal/navigation'
-import { useAppDispatch } from '@internal/store'
 import { AriesSelectors, useAgentSelector } from '@internal/store/aries'
 import { formatToDate, getCredentialDisplayName } from '@internal/utils'
 import { convertToHumanFriendlyName } from '@internal/utils/attribute'
@@ -23,65 +18,44 @@ export interface CredentialDetailScreenProps {
 export const CredentialDetailScreen: React.FunctionComponent<CredentialDetailScreenProps> = ({ route }) => {
   const { t } = useTranslation()
   const navigation = useAppStackNavigation()
-  const bottomSheetModalRef = useRef<BottomSheetModal>(null)
   const credentialId = route.params.credentialId
   const { credential, connection } = useAgentSelector(AriesSelectors.credentialWithConnectionByIdSelector(credentialId))
-  const dispatch = useAppDispatch()
 
   useEffect(() => {
     if (!credential) {
       return
     }
-    const onShowDeleteCredential = () => {
-      bottomSheetModalRef.current?.present()
-    }
 
     navigation.setOptions({
-      headerRight: () => <HeaderIconButton onPress={onShowDeleteCredential} iconType="trash-outline" />,
       title: getCredentialDisplayName(credential.metadata.schemaId),
     })
   }, [navigation, credential])
-
-  const onDeleteCredential = () => {
-    if (credential) {
-      void dispatch(CredentialsThunks.deletCredential(credential.id))
-    }
-    bottomSheetModalRef.current?.close()
-    navigation.goBack()
-  }
 
   if (!credential) {
     return null
   }
 
   return (
-    <>
-      <ScrollViewPage>
-        {connection && (
-          <CredentialMetadata
-            i18nKey="feature.credentials.text.meta"
-            connectionRecord={connection}
-            credentialName={getCredentialDisplayName(credential.metadata.schemaId)}
-            issueDate={formatToDate(credential.createdAt, t('months', { returnObjects: true }))}
-          />
-        )}
-        {credential.credentialAttributes?.map((attribute) => {
-          if (attribute.value) {
-            return (
-              <FormDetail
-                text={attribute.value}
-                headingText={convertToHumanFriendlyName(attribute.name)}
-                key={attribute.name}
-              />
-            )
-          }
-        })}
-      </ScrollViewPage>
-      <DeleteBottomSheet
-        bottomSheetModalRef={bottomSheetModalRef}
-        onDelete={onDeleteCredential}
-        description={`${t('feature.credentials.text.delete')}`}
-      />
-    </>
+    <ScrollViewPage>
+      {connection && (
+        <CredentialMetadata
+          i18nKey="feature.credentials.text.meta"
+          connectionRecord={connection}
+          credentialName={getCredentialDisplayName(credential.metadata.schemaId)}
+          issueDate={formatToDate(credential.createdAt, t('months', { returnObjects: true }))}
+        />
+      )}
+      {credential.credentialAttributes?.map((attribute) => {
+        if (attribute.value) {
+          return (
+            <FormDetail
+              text={attribute.value}
+              headingText={convertToHumanFriendlyName(attribute.name)}
+              key={attribute.name}
+            />
+          )
+        }
+      })}
+    </ScrollViewPage>
   )
 }
--- src/store/app/app.selectors.ts
+++ src/store/app/app.selectors.ts
@@ -2,6 +2,7 @@ import type { AppState } from './app.reducer'
 
 const AppSelectors = {
   isInitializedSelector: (state: { app: AppState }) => state.app.isInitialized,
+  hasEmergencySelector: (state: { app: AppState }) => state.app.hasEmergency,
 }
 
 export { AppSelectors }
